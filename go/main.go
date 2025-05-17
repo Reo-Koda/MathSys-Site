4104114138e43d14a35a0f6058828714513434ed
Reo-Koda <matoryoshika719@gmail.com>
2025-05-17 21:08:55 +0900

    modify go file

--- go/main.go
+++ go/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
+	dsn := "${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(mathsys-site-mysql-1:3306)/mathsys_db?charset=utf8mb4&parseTime=True&loc=Asia%2FTokyo"
+
 	e := gin.Default()
 	e.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
