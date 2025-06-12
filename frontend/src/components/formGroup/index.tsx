dfe553165c30b8287ed343a1af45f8341913f1ff
Reo-Koda <matoryoshika719@gmail.com>
2025-06-12 22:08:14 +0900

    modify formgroup

--- frontend/src/components/formGroup/index.tsx
+++ frontend/src/components/formGroup/index.tsx
@@ -2,20 +2,20 @@ import { JSX } from "react";
 import styles from "./styles.module.css";
 
 type Props ={
-    labeltitle: string
-    nametitle:string 
-    value: string
-    handleChange: (event: React.ChangeEvent<HTMLInputElement>) =>void
-    required: boolean;
+	labeltitle: string
+	nametitle:string 
+	value: string
+	handleChange: (event: React.ChangeEvent<HTMLInputElement>) =>void
+	required: boolean;
 }
 const FormGroup = ({ labeltitle, nametitle, value, handleChange, required }: Props): JSX.Element =>{
-    return(
-        <>
-        <div className={styles.formgroup}>
-            <label htmlFor={labeltitle}>{nametitle}</label>
-            <input id={labeltitle} name={labeltitle} value={value} onChange={handleChange} required={required}/>
-        </div>
-        </>
-    )
+	return(
+		<>
+		<div className={styles.formgroup}>
+			<label htmlFor={labeltitle}>{nametitle}</label>
+			<input id={labeltitle} name={labeltitle} value={value} onChange={handleChange} required={required}/>
+		</div>
+		</>
+	)
 }
 export default FormGroup;
